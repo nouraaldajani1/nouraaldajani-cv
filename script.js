c6b1bd83154547343c71b8707af284ea7cb290ab
nouraaldajani1 <nora_m117@hotmail.com>
2026-05-15 03:07:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// Function for project button
+
+function showMessage() {
+
+    alert("This project reflects my skills in programming, problem solving, and design.");
+
+}
+
+
+// Contact message alert
+
+function sendMessage() {
+
+    alert("Your message has been sent successfully!");
+
+}
+
+
+// Dark mode function
+
+function darkMode() {
+
+    document.body.classList.toggle("dark-mode");
+
+}
+
+
+// Scroll to top function
+
+function topFunction() {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+}
+
